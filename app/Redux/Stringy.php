64f4b17cab190363f1010b32f5ce02f5a1213ab9
Paul Dredge <dredgos@googlemail.com>
2021-02-10 17:25:38 +0000

    Challenge 3 completed

--- app/Redux/Stringy.php
+++ app/Redux/Stringy.php
@@ -1,4 +1,43 @@
 <?php
 
-namespace App\Marsupials;
+namespace App\Redux;
 
+class Stringy 
+{
+    private $string;
+
+    public function __construct($string)
+    {
+        $this->string = $string;
+    }
+
+    public function lower()
+    {
+        $this->string = strtolower($this->string);
+        return $this;
+    }
+
+    public function repeat($num)
+    {
+        $this->string = str_repeat($this->string, $num);
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->string;
+    }
+
+    public function upper()
+    {
+        $this->string = strtoupper($this->string);
+        return $this;
+    }
+
+    public function append($str)
+    {
+        $this->string = $this->string.$str;
+        return $this;
+    }
+
+}
